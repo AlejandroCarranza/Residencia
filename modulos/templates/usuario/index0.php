594a19c9bd1e8276be8a881869a48a69e6438623
alejandro <rulexandro@gmail.com>
2014-12-08 14:51:45 -0600

    no se que rayos es esto

--- modulos/templates/usuario/index0.php
+++ modulos/templates/usuario/index0.php
@@ -3,43 +3,61 @@ include_once '../../includes/db_connect.php';
 include_once '../../includes/functions.php';
  
 sec_session_start();
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title>Directorio en línea</title>
-	<link rel="stylesheet" href="css/estilos.css">
+	<title>Directorio en línea (PRUEBAS)</title>
 </head>
 <body>
-	<?php if ((login_check($mysqli) == true) && ($_SESSION['type'] == '0')): ?>
+	<?php if ((login_check($mysqli) == true) && (($_SESSION['type'] == '1')||($_SESSION['type'] == '2'))): ?>
 	<header>
-		<div class="logos">
-			<img src="images/logo.png" alt="Prospera" class="log">
-			<img src="images/gobierno.png" alt="Gobierno de la republica">
-		</div>
 		<div class="titular">
 			<h1 class="titulo">Nombre del sistema</h1>
 		</div>
-		<div class="usuario">
-			<figure class="avatar">
-					<img src="images/avatar.jpg" alt="mi foto" />
-			</figure>
-			<span class="name">Invitado</span>
-			<a class="cerrarSesion" href="includes/logout.php">Cerrar Sesión</a>
-		</div>
 	</header>
-	<nav>
-		<ul class="menu">
-			<li><a href="">Inicio</a></li>
-			<li><a href="">Registrar</a></li>
-			<li><a href="">Consultar</a></li>
-			<li><a href="">Reportes</a></li>
-			<li><a href="">Administrar</a></li>
-		</ul>
-	</nav>
 	<div class="contenido">
 		CLASE 0
+ <table name="tabla_partidos" width="500" border="1">
+    <tr>
+    <th scope="col">ID</th>
+    <th scope="col">Nombre</th>
+    <th scope="col">Siglas</th>
+			<?php
+			$consultaSub = $mysqli->prepare("SELECT id_partido, nombre_partido, siglas FROM partidos");
+			$consultaSub->execute();
+			$consultaSub->bind_result($id_partido, $nombre_partido, $siglas);
+			$consultaSub->store_result();
+			while($consultaSub->fetch()){ ?>
+			<tr>
+			<td><?php echo $id_partido; ?></td>
+			<td><?php echo utf8_encode($nombre_partido); ?></td>
+			<td><?php echo $siglas; ?></td>
+			<?php }
+			echo "</tr";
+			?>
+</table>
+ <table name="tabla_contactos" width="500" border="1">
+    <tr>
+    <th scope="col">ID</th>
+    <th scope="col">Nombre</th>
+    <th scope="col">Puesto</th>
+			<?php
+			$consultaSub = $mysqli->prepare("SELECT id_contacto, nombre, apellido_paterno, apellido_materno, pc FROM contactos");
+			$consultaSub->execute();
+			$consultaSub->bind_result($id_contacto, $nombre, $apellido_paterno, $apellido_materno, $pc);
+			$consultaSub->store_result();
+			while($consultaSub->fetch()){ ?>
+			<tr>
+			<td><?php echo $id_contacto; ?></td>
+			<td><?php echo utf8_encode($nombre." ".$apellido_paterno." ".$apellido_materno); ?></td>
+			<td><?php echo $pc; ?></td>
+			<?php }
+			echo "</tr";
+			?>
+</table>
 	</div>
 	        <?php else : ?>
             <p>
